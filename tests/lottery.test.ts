058a9fe7d3f470fafb4ddd40c1093c5cba358092
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-22 23:17:21 +0530

    buy tokens
    - integrate token contract into the lottery contract
    - use a payable function in the lottery contract to receive ether
    - then mint tokens equivalent to this ether received for the sender in the token contract

--- tests/lottery.test.ts
+++ tests/lottery.test.ts
@@ -1,10 +1,12 @@
 import { Lottery } from '../typechain-types'
+import { LotteryToken } from '../typechain-types'
 import { ethers } from 'hardhat'
 import { expect } from 'chai'
 import { BigNumber } from 'ethers'
 
 describe('Lottery', () => {
   let lotteryContract: Lottery
+  let lotteryTokenContract: LotteryToken
 
   let BET_PRICE: Number
   let BET_FEE: Number
@@ -31,12 +33,24 @@ describe('Lottery', () => {
     lotteryContract = await lotteryContractFactory.deploy(
       BET_PRICE_DEPLOY_FRIENDLY_FORMAT,
       BET_FEE_DEPLOY_FRIENDLY_FORMAT,
+      'Lottery Token',
+      'LT',
     )
     await lotteryContract.deployed()
+
+    const lotteryTokenAddress = await lotteryContract.lotteryTxnToken()
+
+    const lotteryTokenContractFactory = await ethers.getContractFactory(
+      'LotteryToken',
+    )
+
+    lotteryTokenContract = lotteryTokenContractFactory.attach(
+      lotteryTokenAddress,
+    )
   })
 
   describe('Owner start lottery and define betting duration and fee', () => {
-    it('check owner, betPrice and betFee', async () => {
+    it('validate owner, betPrice and betFee', async () => {
       const [deployer] = await ethers.getSigners()
 
       expect(await lotteryContract.owner()).to.eq(deployer.address)
@@ -56,7 +70,7 @@ describe('Lottery', () => {
 
       await expect(
         lotteryContract.startLottery(closingEpochInSeconds),
-      ).to.be.revertedWith('Closing time must be in the future')
+      ).to.be.revertedWith('Lottery: Closing time must be in the future')
     })
 
     it('Define a block timestamp target (for closing lottery bets), upon lottery start by Owner', async () => {
@@ -74,7 +88,19 @@ describe('Lottery', () => {
     })
   })
 
-  describe('Players must buy ERC20 with ETH', () => {})
+  describe('Players must buy ERC20 with ETH', () => {
+    it('validates players buying lottery token', async () => {
+      const [, playerA] = await ethers.getSigners()
+
+      await lotteryContract
+        .connect(playerA)
+        .sellLotteryTokens({ value: ethers.utils.parseEther('1') })
+
+      expect(await lotteryTokenContract.balanceOf(playerA.address)).to.eq(
+        ethers.utils.parseEther('1'),
+      )
+    })
+  })
   describe('Players pay ERC20 to bet, Only possible before block timestamp met', () => {})
   describe('Anyone can roll the lottery, Only after block timestamp target is met', () => {})
   describe('Winner receives the pooled ERC20 minus fee', () => {})
