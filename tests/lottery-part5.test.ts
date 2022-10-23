c6a0072ceb581f697b74fca0336a5e5e1df7dd18
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-23 11:27:04 +0530

    final draft - burn to ETH implemented

--- tests/lottery-part5.test.ts
+++ tests/lottery-part5.test.ts
@@ -63,5 +63,137 @@ describe('Lottery', () => {
     )
   })
 
-  describe('Players can burn ERC20 tokens and redeem ETH', () => {})
+  describe('Players can burn ERC20 tokens and redeem ETH', () => {
+    it('validate one lottery lifecycle run and burn lottery tokens to ETH in player wallets', async () => {
+      const [
+        deployer,
+        playerA,
+        playerB,
+        playerC,
+        playerD,
+      ] = await ethers.getSigners()
+
+      lotteryStartEpochInSeconds = currentEpoch()
+
+      const lotteryEndEpochInSeconds =
+        lotteryStartEpochInSeconds + LOTTERY_DURATION_IN_SECONDS
+
+      // https://ethereum.stackexchange.com/a/112809
+      const newTimestampInSeconds =
+        lotteryStartEpochInSeconds + LOTTERY_DURATION_IN_SECONDS * 2
+
+      await lotteryContract
+        .connect(playerA)
+        .sellLotteryTokens({ value: ethers.utils.parseEther('10') })
+
+      await lotteryTokenContract
+        .connect(playerA)
+        .approve(
+          lotteryContract.address,
+          await lotteryTokenContract.balanceOf(playerA.address),
+        )
+
+      await lotteryContract
+        .connect(playerB)
+        .sellLotteryTokens({ value: ethers.utils.parseEther('15') })
+
+      await lotteryTokenContract
+        .connect(playerB)
+        .approve(
+          lotteryContract.address,
+          await lotteryTokenContract.balanceOf(playerB.address),
+        )
+
+      await lotteryContract
+        .connect(playerC)
+        .sellLotteryTokens({ value: ethers.utils.parseEther('20') })
+      await lotteryTokenContract
+        .connect(playerC)
+        .approve(
+          lotteryContract.address,
+          await lotteryTokenContract.balanceOf(playerC.address),
+        )
+
+      await lotteryContract
+        .connect(playerD)
+        .sellLotteryTokens({ value: ethers.utils.parseEther('25') })
+
+      await lotteryTokenContract
+        .connect(playerD)
+        .approve(
+          lotteryContract.address,
+          await lotteryTokenContract.balanceOf(playerD.address),
+        )
+
+      const playerAddresses = [
+        playerA.address,
+        playerB.address,
+        playerC.address,
+        playerD.address,
+      ]
+
+      await lotteryContract.startLottery(
+        lotteryEndEpochInSeconds,
+        BASE_WINNING_FEE_DEPLOY_FRIENDLY_FORMAT,
+      )
+
+      await lotteryContract.connect(playerA).bet()
+      await lotteryContract.connect(playerB).bet()
+      await lotteryContract.connect(playerC).bet()
+      await lotteryContract.connect(playerA).bet()
+      await lotteryContract.connect(playerD).bet()
+      await lotteryContract.connect(playerC).bet()
+      await lotteryContract.connect(playerB).bet()
+
+      await ethers.provider.send('evm_mine', [newTimestampInSeconds])
+
+      await lotteryContract.connect(deployer).endLottery()
+
+      const winnerAddress = await lotteryContract.latestLotteryWinner()
+      const winningAccount = await ethers.getSigner(winnerAddress)
+      const winnerAccountBalanceLotteryBeforeLotteryTokenBurn = await winningAccount.getBalance()
+
+      const unclaimedWinningAmount = await lotteryContract.winningStash(
+        winnerAddress,
+      )
+
+      const [, calculatedWinningFee] = calculateWinningFee(
+        unclaimedWinningAmount,
+      )
+
+      const winnerTokenBalanceBeforeClaim = await lotteryTokenContract.balanceOf(
+        winnerAddress,
+      )
+      await lotteryContract
+        .connect(winningAccount)
+        .withdrawWinning(calculatedWinningFee)
+
+      const winnerTokenBalanceAfterClaim = await lotteryTokenContract.balanceOf(
+        winnerAddress,
+      )
+
+      expect(winnerTokenBalanceAfterClaim.gt(winnerTokenBalanceBeforeClaim)).to
+        .be.true
+
+      const amountToBurn = winnerTokenBalanceAfterClaim
+
+      await lotteryContract
+        .connect(winningAccount)
+        .trackLatestBurn(winnerTokenBalanceAfterClaim)
+
+      await lotteryTokenContract
+        .connect(winningAccount)
+        .burn(winnerTokenBalanceAfterClaim)
+
+      await lotteryContract.connect(winningAccount).withdrawLastBurnToEther()
+
+      const winnerAccountBalanceLotteryAfterLotteryTokenBurn = await winningAccount.getBalance()
+
+      expect(
+        winnerAccountBalanceLotteryAfterLotteryTokenBurn.gt(
+          winnerAccountBalanceLotteryBeforeLotteryTokenBurn,
+        ),
+      ).to.be.true
+    })
+  })
 })
