5a82d3ff0984ad8d7dddad6582a4fc705deb4afe
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-27 22:12:01 +0530

    Update calculateWinningFee.ts
    update implementation for calculating the calculating fee as a percentage of winning amount

--- scripts/helpers/calculateWinningFee.ts
+++ scripts/helpers/calculateWinningFee.ts
@@ -1,6 +1,9 @@
 import { ethers } from 'ethers'
 
-export default (winningAmount: ethers.BigNumber): ethers.BigNumber[] => [
-  winningAmount.sub(winningAmount.div(30)),
-  winningAmount.div(30),
-]
+export default (winningAmount: ethers.BigNumber): ethers.BigNumber[] => {
+  const feePercentage = 30
+  return [
+    winningAmount.mul(ethers.BigNumber.from(100 - feePercentage)),
+    winningAmount.mul(ethers.BigNumber.from(feePercentage)),
+  ]
+}
