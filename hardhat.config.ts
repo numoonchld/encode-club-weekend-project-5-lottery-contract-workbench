33ccea0802ce54fa8788ca97769aea38fd25a5dc
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-29 19:31:28 +0530

    goerli deployment config and script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,13 +4,22 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 require('@nomicfoundation/hardhat-chai-matchers')
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const config: HardhatUserConfig = {
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
   solidity: {
     version: '0.8.17',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 4000,
       },
     },
   },
